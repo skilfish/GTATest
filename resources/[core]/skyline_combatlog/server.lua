1a061466fda93ed47e7e5c7c7835ed843eaf2812
skilfish <skilfin.gaming@t-online.de>
2025-07-08 23:04:24 +0200

    Phase 10.2 – Alte Disconnect-Handler bereinigt

--- resources/[core]/skyline_combatlog/server.lua
+++ resources/[core]/skyline_combatlog/server.lua
@@ -1,13 +1,1 @@
-AddEventHandler("playerDropped", function(reason)
-    local crds = GetEntityCoords(GetPlayerPed(source))
-    local id = source
-    local identifier = ""
-    if Config.UseSteam then
-        identifier = GetPlayerIdentifier(source, 0)
-    else
-        identifier = GetPlayerIdentifier(source, 1)
-    end
-    TriggerClientEvent("skyline_combatlog", -1, id, crds, identifier, reason)
-   
-end)
-
+-- Alte playerDropped-Logik entfernt. Verwende core_disconnect.
