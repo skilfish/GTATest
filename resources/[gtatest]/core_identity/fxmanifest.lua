3c18b2375c52da8427116ee221502d5ff5ea6102
skilfish <skilfin.gaming@t-online.de>
2025-07-08 21:49:27 +0200

    Phase 8 erweitert: Autostart der Charaktererstellung bei Join

---
+++ resources/[gtatest]/core_identity/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- fxmanifest for core_identity
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Spieleridentität & Charakterverwaltung'
+author 'Optimiert für skilfish'
+
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
